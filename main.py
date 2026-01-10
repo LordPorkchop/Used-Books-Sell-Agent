374893c458bbb9da439597afe9f472a552617d96
Lord Porkchop <149256533+LordPorkchop@users.noreply.github.com>
2026-01-10 15:31:55 +0100

    Update main.py
    Added start_pülaywright() and stop_playwright()

--- main.py
+++ main.py
@@ -1,7 +1,58 @@
-from playwright.sync_api import sync_playwright, TimeoutError
 import logging
-from flask import Flask
 import os
+
+from flask import Flask
+from playwright.sync_api import sync_playwright, TimeoutError, Browser, BrowserContext
+from typing import Literal
+
+
+def start_playwright(
+    browsertype: Literal["chromium", "firefox", "webkit"] = "chromium",
+    use_obfuscation_headers: bool = True,
+) -> tuple[Browser, BrowserContext]:
+    """Starts playwright browser and returns browser as well as context
+
+    Args:
+        browsertype (Literal[&quot;chromium&quot;, &quot;firefox&quot;, &quot;webkit&quot;], optional): Which browser type to use. Defaults to "chromium".
+        use_obfuscation_headers (bool, optional): Whether to use obfuscated headers (tricks most bot detection). Defaults to True.
+
+    Raises:
+        ValueError: If the passed brwoser type is invalid
+
+    Returns:
+        tuple[Browser, BrowserContext]: The created browser along with the created context
+    """
+    with sync_playwright() as p:
+        match browsertype:
+            case "chromium":
+                browser = p.chromium.launch()
+            case "firefox":
+                browser = p.firefox.launch()
+            case "webkit":
+                browser = p.webkit.launch()
+            case _:
+                raise ValueError('Invalid browser type "{}"'.format(browsertype))
+
+        if use_obfuscation_headers:
+            context = browser.new_context(
+                locale="de-DE",
+                timezone_id="Europe/Berlin",
+                user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+                extra_http_headers={
+                    "Accept-Language": "de-DE,de;q=0.9",
+                },
+            )
+        else:
+            context = browser.new_context()
+
+        return browser, context
+
+
+def stop_playwright(browser: Browser):
+    try:
+        browser.close()
+    except:
+        raise ValueError('"{}" cannot be closed since it is not open'.format(browser))
 
 
 def rebuy(
