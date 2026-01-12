1c6d7dae4172a3dff381f417becd43c9082e5bba
Lord Porkchop <149256533+LordPorkchop@users.noreply.github.com>
2026-01-12 23:02:16 +0100

    Added /info/... URL path
    - Introduced new app route /info/<isbn> to fetch title, author and release year of a book

--- main.py
+++ main.py
@@ -58,28 +58,6 @@ def stop_playwright(browser: Browser):
         raise ValueError('"{}" cannot be closed since it is not open'.format(browser))
 
 
-def get_book_info(isbn: str) -> tuple | None:
-    isbn = isbn.replace("-", "")
-    if not isbn.isdigit() or len(isbn) not in [10, 12, 13, 15, 16]:
-        raise ValueError("Invalid isbn: {}".format(isbn))
-    try:
-        response = requests.get(
-            f"https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}",
-        )
-        response.raise_for_status()
-    except:
-        return None
-    else:
-        book_data = response.content.decode()
-        book_data_dir = json.loads(book_data)
-        if book_data_dir["totalItems"] == 0:
-            return None
-        book = book_data_dir["items"][0]
-        book_info = book["volumeInfo"]
-        publish_year = book_info["publishedDate"].split("-")[0]
-        return book_info["title"], book_info["authors"], publish_year
-
-
 def rebuy(context: BrowserContext, isbn: str) -> float:
     if not isbn.isdigit() or len(isbn) not in [10, 12, 13, 15, 16]:
         raise ValueError("Invalid ISBN")
@@ -418,6 +396,37 @@ def getPrice_all(isbn: str):  # type: ignore
             "rebuy_price": str(rebuy_price),
             "momox_price": str(momox_price),
             "buchmaxe_price": str(buchmaxe_price),
+        }, 200
+
+
+@app.route("/info/<isbn>")
+def get_book_info(isbn: str):
+    isbn = isbn.replace("-", "")
+    if not isbn.isdigit() or len(isbn) not in [10, 12, 13, 15, 16]:
+        raise ValueError("Invalid isbn: {}".format(isbn))
+    try:
+        response = requests.get(
+            f"https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}",
+        )
+        response.raise_for_status()
+    except:
+        return {
+            "status_code": 500,
+            "message": "An internal server error occured during the processing of the request",
+        }, 500
+    else:
+        book_data = response.content.decode()
+        book_data_dir = json.loads(book_data)
+        if book_data_dir["totalItems"] == 0:
+            return {"status_code": 422, "message": "Invalid ISBN"}, 422
+        book = book_data_dir["items"][0]
+        book_info = book["volumeInfo"]
+        publish_year = book_info["publishedDate"].split("-")[0]
+        return {
+            "title": book_info["title"],
+            "authors": book_info["authors"],
+            "published_year": publish_year,
+            "status_code": 200,
         }, 200
 
 
